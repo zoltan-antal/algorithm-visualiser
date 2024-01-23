b12bea4efd223d0e99b7eb21f3a63df7df217a56
zolantal <zoltan_antal@outlook.com>
2024-01-23 15:15:43 +0000

    Create array generator function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
+  const startArray = generateArray(50, 50);
   return <h1>Algorithm Visualiser</h1>;
 }
+
+const generateArray = (length: number, range: number) => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr[i] = Math.floor(Math.random() * (range + 1));
+  }
+  return arr;
+};
 
 export default App;
