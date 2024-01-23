6b2af8316a82c5a061b9f2fc0e31a57785c525a0
zolantal <zoltan_antal@outlook.com>
2024-01-23 16:09:37 +0000

    Add comments to bubble sort

--- src/App.tsx
+++ src/App.tsx
@@ -43,10 +43,13 @@ const bubbleSort = (unsorted: number[]) => {
   const sorted = [...unsorted];
   const n = sorted.length;
 
+  // Continue while at least one swap is made, i.e., the array is unsorted
   let swap = true;
   while (swap) {
     swap = false;
+    // Iterate through the whole array
     for (let j = 0; j < n - 1; j++) {
+      // Swap neighbouring elements if they're out of order
       if (sorted[j] > sorted[j + 1]) {
         swap = true;
         [sorted[j], sorted[j + 1]] = [sorted[j + 1], sorted[j]];
