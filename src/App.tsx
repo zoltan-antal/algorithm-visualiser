c25c24bede88e342006e774fb1ba4e138162a8f6
zolantal <zoltan_antal@outlook.com>
2024-01-23 15:59:12 +0000

    Add selection sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
   const startArray = generateArray(50, 50);
+  console.log('Start');
+  console.log(startArray);
+  console.log('Selection sort');
+  console.log(selectionSort(startArray));
   return <h1>Algorithm Visualiser</h1>;
 }
 
@@ -9,6 +13,30 @@ const generateArray = (length: number, range: number) => {
     arr[i] = Math.floor(Math.random() * (range + 1));
   }
   return arr;
+};
+
+const selectionSort = (unsorted: number[]) => {
+  const sorted = [...unsorted];
+  const n = sorted.length;
+
+  for (let i = 0; i < n; i++) {
+    // Assume the current index is the minimum
+    let min_index = i;
+
+    // Find the index of the minimum element in the unsorted part
+    for (let j = i + 1; j < n; j++) {
+      if (sorted[j] < sorted[min_index]) {
+        min_index = j;
+      }
+    }
+
+    // Swap the found element with the element at the current index
+    const temp = sorted[i];
+    sorted[i] = sorted[min_index];
+    sorted[min_index] = temp;
+  }
+
+  return sorted;
 };
 
 export default App;
