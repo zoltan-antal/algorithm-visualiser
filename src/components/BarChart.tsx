43e38b767b3528fdc865164084ac769fdb198955
zolantal <zoltan_antal@outlook.com>
2024-01-24 17:39:39 +0000

    Colour given index in different colour

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,6 +1,18 @@
 import { BarChart, Bar } from 'recharts';
 
 const BarChartComponent = ({ data }: any) => {
+  const targetIndex = -1;
+
+  const renderCustomBar = (props: any) => {
+    const { index } = props;
+
+    return (
+      <g>
+        <rect {...props} fill={index === targetIndex ? 'red' : '#8884d8'} />
+      </g>
+    );
+  };
+
   return (
     <BarChart
       width={300}
@@ -8,7 +20,7 @@ const BarChartComponent = ({ data }: any) => {
       data={data.map((value: any, index: any) => ({ index: index + 1, value }))}
       margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
     >
-      <Bar dataKey="value" fill="#8884d8" />
+      <Bar dataKey="value" shape={renderCustomBar} />
     </BarChart>
   );
 };
