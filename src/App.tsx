c7a4bacf931a79abd0ae1b0a8d0e356b3370868c
zolantal <zoltan_antal@outlook.com>
2024-01-24 14:33:08 +0000

    Update bubbleSort and transform it to use states

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 
 interface SelectionSortState {
+  processing: boolean;
   arr: number[];
   n: number;
   i: number;
   j: number;
   min_index: number;
+}
+
+interface BubbleSortState {
   processing: boolean;
+  arr: number[];
+  n: number;
+  i: number;
+  j: number;
+  swap: boolean;
 }
 
 interface AlgorithmStates {
   selectionSort: SelectionSortState;
+  bubbleSort: BubbleSortState;
 }
 
 type Arrays = {
@@ -19,21 +29,31 @@ type Arrays = {
 
 function App() {
   const randomArray = generateArray(5, 1, 50);
+  const arrayLength = randomArray.length;
   const [arrays, setArrays] = useState<Arrays>({
     selectionSort: [...randomArray],
+    bubbleSort: [...randomArray],
   });
 
   const runAlgorithms = async () => {
-    const algorithms = [selectionSort];
+    const algorithms = [selectionSort, bubbleSort];
 
     const algorithmStates: AlgorithmStates = {
       selectionSort: {
+        processing: true,
         arr: [...randomArray],
-        n: randomArray.length,
+        n: arrayLength,
         i: 0,
         j: 1,
         min_index: 0,
+      },
+      bubbleSort: {
         processing: true,
+        arr: [...randomArray],
+        n: arrayLength,
+        i: 0,
+        j: 0,
+        swap: true,
       },
     };
 
@@ -41,9 +61,12 @@ function App() {
       for (const algorithm of algorithms) {
         const algorithmName = algorithm.name as keyof typeof algorithmStates;
         const newState = algorithm(algorithmStates[algorithmName]);
-        algorithmStates[algorithmName] = newState;
-        await new Promise((resolve) => setTimeout(resolve, 500));
+        if (!newState.processing) {
+          console.log(algorithmName + ' DONE');
+        }
+        algorithmStates[algorithmName] = newState as any;
       }
+      await new Promise((resolve) => setTimeout(resolve, 500));
 
       setArrays((prevArrays) => {
         const updatedArrays: Arrays = { ...prevArrays };
@@ -57,6 +80,7 @@ function App() {
   };
 
   console.log('Selection sort: ' + arrays.selectionSort);
+  console.log('Bubble sort: ' + arrays.bubbleSort);
 
   return (
     <>
@@ -96,8 +120,12 @@ const generateArray = (length: number, min: number, max: number) => {
 //   return sorted;
 // };
 
-const selectionSort = (inputState: AlgorithmStates['selectionSort']) => {
-  const state = structuredClone(inputState);
+const selectionSort = (inputState: any) => {
+  if (!inputState.processing) {
+    return inputState;
+  }
+
+  const state = structuredClone(inputState) as SelectionSortState;
 
   if (state.arr[state.j] < state.arr[state.min_index]) {
     state.min_index = state.j;
@@ -126,19 +154,49 @@ const selectionSort = (inputState: AlgorithmStates['selectionSort']) => {
 
 //   // Continue while at least one swap is made, i.e., the array is unsorted
 //   let swap = true;
+//   let i = 0;
 //   while (swap) {
 //     swap = false;
 //     // Iterate through the whole array
-//     for (let j = 0; j < n - 1; j++) {
+//     for (let j = 0; j < n - i - 1; j++) {
 //       // Swap neighbouring elements if they're out of order
 //       if (sorted[j] > sorted[j + 1]) {
 //         swap = true;
 //         [sorted[j], sorted[j + 1]] = [sorted[j + 1], sorted[j]];
 //       }
 //     }
+//     i++;
 //   }
 
 //   return sorted;
 // };
+
+const bubbleSort = (inputState: any) => {
+  if (!inputState.processing) {
+    return inputState;
+  }
+
+  const state = structuredClone(inputState) as BubbleSortState;
+
+  if (state.arr[state.j] > state.arr[state.j + 1]) {
+    state.swap = true;
+    [state.arr[state.j], state.arr[state.j + 1]] = [
+      state.arr[state.j + 1],
+      state.arr[state.j],
+    ];
+  }
+  if (state.j >= state.n - 2 - state.i) {
+    if (!state.swap) {
+      state.processing = false;
+      return state;
+    }
+    state.i++;
+    state.j = 0;
+    state.swap = false;
+    return state;
+  }
+  state.j++;
+  return state;
+};
 
 export default App;
