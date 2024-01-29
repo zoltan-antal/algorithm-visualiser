399e10e209b1f6e86084103ed168b10bc45dc5e9
zolantal <zoltan_antal@outlook.com>
2024-01-29 17:02:22 +0000

    Add sliders for array size, timeout & value range

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,24 @@ import quickSort from './algorithms/quickSort.ts';
 import AlgorithmStates from './types/AlgorithmStates.ts';
 
 function App() {
-  const TIMEOUT = 10;
-  const MAX_VALUE = 100;
-  const ARRAY_SIZE = 50;
+  const DEFAULT_ARRAY_SIZE = 40;
+  const MIN_ARRAY_SIZE = 10;
+  const MAX_ARRAY_SIZE = 100;
+  const ARRAY_SIZE_STEP = 10;
+
+  const DEFAULT_DELAY = 50;
+  const MIN_DELAY = 25;
+  const MAX_DELAY = 500;
+  const DELAY_STEP = 25;
+
+  const DEFAULT_MAX_VALUE = 50;
+  const MIN_MAX_VALUE = 5;
+  const MAX_MAX_VALUE = 100;
+  const MAX_VALUE_STEP = 5;
+
+  const [arraySize, setArraySize] = useState(DEFAULT_ARRAY_SIZE);
+  const [delay, setDelay] = useState(DEFAULT_DELAY);
+  const [maxValue, setMaxValue] = useState(DEFAULT_MAX_VALUE);
 
   const algorithms = [
     selectionSort,
@@ -36,7 +51,7 @@ function App() {
   const [processing, setProcessing] = useState<boolean>(false);
 
   const generateArrays = () => {
-    const randomArray: number[] = generateArray(ARRAY_SIZE, 1, MAX_VALUE);
+    const randomArray: number[] = generateArray(arraySize, 1, maxValue);
     setAlgorithmStates((prevStates) => {
       const updatedStates: AlgorithmStates = structuredClone(prevStates);
       Object.keys(updatedStates).forEach(
@@ -51,29 +66,76 @@ function App() {
   };
   useEffect(() => {
     generateArrays();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
       <h1>Algorithm Visualiser</h1>
-      <button
-        onClick={() =>
-          runAlgorithms(
-            algorithms,
-            algorithmStates,
-            setAlgorithmStates,
-            TIMEOUT,
-            setProcessing
-          )
-        }
-        disabled={processing}
-      >
-        Run
-      </button>
-      <button onClick={() => generateArrays()} disabled={processing}>
-        Regenerate
-      </button>
-      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+        <div>
+          <input
+            type="range"
+            min={MIN_ARRAY_SIZE}
+            max={MAX_ARRAY_SIZE}
+            step={ARRAY_SIZE_STEP}
+            value={arraySize}
+            onChange={(e) => setArraySize(Number(e.target.value))}
+          />
+          <p>Array size: {arraySize}</p>
+        </div>
+        <div>
+          <input
+            type="range"
+            min={MIN_DELAY}
+            max={MAX_DELAY}
+            step={DELAY_STEP}
+            value={delay}
+            onChange={(e) => setDelay(Number(e.target.value))}
+          />
+          <p>Timeout: {delay}ms</p>
+        </div>
+        <div>
+          <input
+            type="range"
+            min={MIN_MAX_VALUE}
+            max={MAX_MAX_VALUE}
+            step={MAX_VALUE_STEP}
+            value={maxValue}
+            onChange={(e) => setMaxValue(Number(e.target.value))}
+          />
+          <p>Value range: {maxValue}</p>
+        </div>
+        <button
+          onClick={() => {
+            setDelay(DEFAULT_DELAY);
+            setMaxValue(DEFAULT_MAX_VALUE);
+            setArraySize(DEFAULT_ARRAY_SIZE);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            runAlgorithms(
+              algorithms,
+              algorithmStates,
+              setAlgorithmStates,
+              delay,
+              setProcessing
+            )
+          }
+          disabled={processing}
+        >
+          Run
+        </button>
+        <button onClick={() => generateArrays()} disabled={processing}>
+          Regenerate
+        </button>
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         <div>
           <h2>Selection sort</h2>
           <BarChart data={algorithmStates.selectionSort} />
