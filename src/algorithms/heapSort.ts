e74715392b86174132bd3e5cbd4db616c9c28200
zolantal <zoltan_antal@outlook.com>
2024-04-11 17:36:02 +0100

    Simplify heap sort logic & improve comments

--- src/algorithms/heapSort.ts
+++ src/algorithms/heapSort.ts
@@ -6,37 +6,42 @@ const heapSort = (unsorted: number[]) => {
   const steps: AlgorithmState[] = [];
 
   // Initialise variables for the heap building phase
-  let start = Math.floor(n / 2);
-  let end = n;
+  let start = Math.floor(n / 2); // last non-leaf node + 1 ((n - 2) / 2 + 1)
+  let end = n - 1;
 
-  while (end > 1) {
-    // Heap construction
+  while (end > 0) {
+    // HEAP CONSTRUCTION
     if (start > 0) {
+      // Decrement start
+      // - the element at start will be percolated down during Heapify
       start--;
-    } /* Heap extraction */ else {
-      end--;
+    } /* HEAP EXTRACTION */ else {
+      // Swap first and last elements of the heap
+      // - the first element will be percolated down during Heapify
       [sorted[0], sorted[end]] = [sorted[end], sorted[0]];
       steps.push({ array: [...sorted], highlights: [start, end] });
+      // Decrement end so that the element moved to the end will remain there
+      end--;
     }
 
-    // Heapify
-    let root = start;
-    while (root * 2 + 1 < end) {
+    // HEAPIFY
+    // Start at the root of the heap
+    let curr = start;
+    while (curr * 2 + 1 <= end) {
       // Select the child with the larger value
-      let child = root * 2 + 1;
-      if (child + 1 < end && sorted[child + 1] > sorted[child]) {
+      let child = curr * 2 + 1;
+      if (child + 1 <= end && sorted[child + 1] > sorted[child]) {
         child++;
       }
       steps.push({
         array: [...sorted],
-        highlights: [start, end - 1, root, child],
+        highlights: [start, end, curr, child],
       });
-      // Swap root with the larger child if necessary
-      if (sorted[root] < sorted[child]) {
-        [sorted[root], sorted[child]] = [sorted[child], sorted[root]];
-        root = child;
-      } else {
-        // Break if the heap property is satisfied
+      // Swap current node with the larger child if necessary
+      if (sorted[child] > sorted[curr]) {
+        [sorted[curr], sorted[child]] = [sorted[child], sorted[curr]];
+        curr = child;
+      } /* Break if the heap property is satisfied */ else {
         break;
       }
     }
