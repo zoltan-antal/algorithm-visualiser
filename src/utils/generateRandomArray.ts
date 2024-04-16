6a99890efbd01c58288ab03ae4429bf566f1fa09
zolantal <zoltan_antal@outlook.com>
2024-04-16 16:04:58 +0100

    Modify random array generation so that the maximum is at least 90% of the range

--- src/utils/generateRandomArray.ts
+++ src/utils/generateRandomArray.ts
@@ -1,9 +1,17 @@
-const generateRandomArray = (length: number, min: number, max: number) => {
+const generateRandomArray = (
+  length: number,
+  min: number,
+  max: number
+): number[] => {
   const arr: number[] = [];
   for (let i = 0; i < length; i++) {
     arr[i] = Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return arr;
+  if (Math.max(...arr) < (max - min) * 0.9) {
+    return generateRandomArray(length, min, max);
+  } else {
+    return arr;
+  }
 };
 
 export default generateRandomArray;
