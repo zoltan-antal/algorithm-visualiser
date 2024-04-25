a6768e453124d716a53dce572ff5c250ea3d89b5
zolantal <zoltan_antal@outlook.com>
2024-04-25 11:17:31 +0100

    Add "noopener noreferrer" to new tab links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,11 +9,16 @@ const Footer = () => {
         className="user"
         href="https://github.com/zoltan-antal"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Zoltan Antal
       </a>
       <pre> </pre>
-      <a href="https://github.com/zoltan-antal" target="_blank">
+      <a
+        href="https://github.com/zoltan-antal"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={githubIcon} alt="GitHub" />
       </a>
       <pre> | </pre>
@@ -21,6 +26,7 @@ const Footer = () => {
         className="repo"
         href="https://github.com/zoltan-antal/algorithm-visualiser"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Source code
       </a>
